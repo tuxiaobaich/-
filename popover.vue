06462a09c75097eb5e847d2557612fd9b8706cf4
tuxiaobaich <52303927+tuxiaobaich@users.noreply.github.com>
2025-05-18 22:12:00 +0800

    Create popover.vue

---
+++ popover.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-popover
+    placement="bottom"
+    trigger="click"
+    v-model:visible="popoverVisible"
+  >
+    <template #reference>
+      <el-input
+        placeholder="点击弹出"
+        readonly
+        class="pointer-input"
+      />
+    </template>
+    <div @click="popoverVisible = false" style="cursor:pointer;">
+      点我关闭弹窗
+    </div>
+    哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
+  </el-popover>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const popoverVisible = ref(false)
+</script>
+
+<style scoped>
+.pointer-input {
+  cursor: pointer;
+}
+.pointer-input :deep(.el-input__wrapper) {
+  cursor: pointer;
+}
+.pointer-input :deep(input) {
+  cursor: pointer;
+}
+</style>
