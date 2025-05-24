96132771be7762ad97279f7a500525fc30316cc9
tuxiaobaich <52303927+tuxiaobaich@users.noreply.github.com>
2025-05-24 20:18:25 +0800

    Create index_上传.vue

---
+++ index_上传.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h2>File Upload</h2>
+      <FileUpload />
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import FileUpload from '@/components/FileUpload.vue'
+  </script>
